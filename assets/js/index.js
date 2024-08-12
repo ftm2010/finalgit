183cd061a59383fa4c91a75f8171024d8aaa7915
Fatemeh Basti <ftmbasti@gmail.com>
2024-08-12 17:21:41 +0330

    start project

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("shuffleMedia");
+const context = canvas.getContext("2d");
+const neverHoodWrap = document.getElementById('aboutHeroSection');
+const frameCount = 8;
+let currentFrame;
